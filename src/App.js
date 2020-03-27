5f52f4184d0f3cd7f7aac09ade5c2c692826bd2e
Rahul Makhija <rahuls360@gmail.com>
2020-03-28 03:47:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import {data} from './data.js';
 
-function App() {
+const App = () => {
+  const [searchString, setSearchString] = useState('');
+
+  useEffect(() => {
+    const result = data.filter(item => item.full_text.toLowerCase().indexOf(searchString.toLowerCase()) > -1);
+    console.log(result, "result");
+  }, [searchString])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Twitter Likes - Search</h3>
+      <input value={searchString} onChange={e => setSearchString(e.target.value)}/>
     </div>
   );
 }
