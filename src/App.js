8ac88d2c6961ede18a389295db98a4ebfffe0091
Rahul Makhija <rahuls360@gmail.com>
2021-02-13 00:15:38 +0530

    add go to top show on little scroll update tweets

--- src/App.js
+++ src/App.js
@@ -4,10 +4,31 @@ import { data } from './data.js';
 import useDebounce from './hooks/useDebounce';
 import Result from './Result';
 
+// copy(Object.values($reactTemp1.getState().entities.tweets.entities))
+
 const App = () => {
   const [searchText, setSearchText] = useState('');
   const [result, setResult] = useState([]);
+  const [showScroll, setShowScroll] = useState(false);
   const searchString = useDebounce(searchText, 300);
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollFunction);
+
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        setShowScroll(true);
+      } else {
+        setShowScroll(false);
+      }
+    }
+    return () => {
+      window.removeEventListener('scroll', scrollFunction);
+    };
+  }, []);
 
   useEffect(() => {
     const newResult = data.filter(
@@ -25,6 +46,19 @@ const App = () => {
         value={searchText}
         onChange={(e) => setSearchText(e.target.value)}
       />
+
+      <div
+        id='go-to-top'
+        onClick={() => {
+          window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+        }}
+        style={{ display: showScroll ? 'unset' : 'none' }}>
+        Go to top
+      </div>
 
       <div className='tweets'>
         {result.map((item) => (
